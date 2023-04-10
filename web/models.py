5c4bc97209f8fbd4e8dae55154016e2d3b4f9905
dgolov <dmitriy.evgenyevich@gmail.com>
2023-04-10 12:31:13 +0300

    Added history model

--- web/models.py
+++ web/models.py
@@ -41,7 +41,7 @@ class Asset(models.Model):
 
 
 class AssetImage(models.Model):
-    asset = models.ForeignKey(Asset, on_delete=models.CASCADE, verbose_name="Изображение", related_name="images")
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE, verbose_name="Актив", related_name="images")
     image = models.ImageField(upload_to="images", verbose_name="Изображенеие")
 
     def __str__(self):
@@ -62,3 +62,25 @@ class Order(models.Model):
     class Meta:
         verbose_name = "Отчет"
         verbose_name_plural = "Отчеты"
+
+
+class History(models.Model):
+    STATUS_CHOICES = ("in_work", "broken", "under_repair", "in_reserve")
+    STATUS_CHOICES_RUS = ("В работе", "Сломано", "В ремонте", "В запасе")
+    STATUS_CHOICES = list(zip(STATUS_CHOICES, STATUS_CHOICES_RUS))
+
+    STATE_CHOICES = ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5))
+
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE, verbose_name="Актив", related_name="history")
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, verbose_name="Местоположение")
+    price = models.DecimalField(verbose_name="Стоимость", decimal_places=2, max_digits=7)
+    state = models.IntegerField(verbose_name="Состояние", choices=STATE_CHOICES)
+    status = models.CharField(max_length=256, verbose_name='Статус', choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время создания")
+
+    def __str__(self):
+        return f"{self.created_at} - {self.asset.name}"
+
+    class Meta:
+        verbose_name = "История"
+        verbose_name_plural = "История"
