87e22eb0a18f3217dd36d41e19bc7095adffdc48
dgolov <dmitriy.evgenyevich@gmail.com>
2023-04-26 15:01:29 +0300

    Added test view

--- web/views.py
+++ web/views.py
@@ -1,3 +1,10 @@
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+
+class MainView(View):
+    """ Представление главной страницы
+    """
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'web/index.html', context)
