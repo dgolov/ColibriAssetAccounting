aa9fa345849e126296e346211f066e422ebda9d5
dgolov <dmitriy.evgenyevich@gmail.com>
2023-04-30 23:21:43 +0300

    Removed cache

--- web/urls.py
+++ web/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 import web.views as views
 
 
 urlpatterns = [
-    path('', cache_page(60)(views.AssetList.as_view()), name='assets'),
-    path('/<int:pk>', cache_page(60)(views.AssetDetail.as_view()), name='assets_detail'),
-    path('locations', cache_page(60)(views.LocationList.as_view()), name='locations'),
-    path('locations/<int:pk>', cache_page(60)(views.LocationDetail.as_view()), name='locations_detail'),
-    path('orders', cache_page(60)(views.OrderList.as_view()), name='orders'),
+    path('', views.AssetList.as_view(), name='assets'),
+    path('/<int:pk>', views.AssetDetail.as_view(), name='assets_detail'),
+    path('locations', views.LocationList.as_view(), name='locations'),
+    path('locations/<int:pk>', views.LocationDetail.as_view(), name='locations_detail'),
+    path('orders', views.OrderList.as_view(), name='orders'),
 ]
