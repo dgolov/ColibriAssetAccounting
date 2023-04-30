f877ba37b9af0206ad4755b0855323186433c69b
dgolov <dmitriy.evgenyevich@gmail.com>
2023-05-01 00:03:28 +0300

    Updated urls

--- web/urls.py
+++ web/urls.py
@@ -5,7 +5,7 @@ import web.views as views
 
 urlpatterns = [
     path('', views.AssetList.as_view(), name='assets'),
-    path('/<int:pk>', views.AssetDetail.as_view(), name='assets_detail'),
+    path('assets/<int:pk>', views.AssetDetail.as_view(), name='assets_detail'),
     path('locations', views.LocationList.as_view(), name='locations'),
     path('locations/<int:pk>', views.LocationDetail.as_view(), name='locations_detail'),
     path('orders', views.OrderList.as_view(), name='orders'),
