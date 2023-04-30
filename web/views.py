4f56aa8d51453dbda3de8c71cd6e5b90451809de
dgolov <dmitriy.evgenyevich@gmail.com>
2023-04-30 20:38:41 +0300

    Updated main view

--- web/views.py
+++ web/views.py
@@ -1,10 +1,14 @@
 from django.views import View
 from django.shortcuts import render
+from web.models import Asset
 
 
 class MainView(View):
     """ Представление главной страницы
     """
     def get(self, request, *args, **kwargs):
-        context = {}
+        context = {
+            "title": "Main page",
+            "assets": Asset.objects.filter(is_active=True)
+        }
         return render(request, 'web/index.html', context)
