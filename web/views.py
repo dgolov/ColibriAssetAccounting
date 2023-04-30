4791289236a392ff4736118cace5abed571b4a86
dgolov <dmitriy.evgenyevich@gmail.com>
2023-04-30 21:01:29 +0300

    Updated views

--- web/views.py
+++ web/views.py
@@ -1,14 +1,19 @@
-from django.views import View
+from django.views.generic import DetailView, ListView, CreateView
 from django.shortcuts import render
 from web.models import Asset
 
 
-class MainView(View):
+class MainView(ListView):
     """ Представление главной страницы
     """
-    def get(self, request, *args, **kwargs):
-        context = {
-            "title": "Main page",
-            "assets": Asset.objects.filter(is_active=True)
-        }
-        return render(request, 'web/index.html', context)
+    model = Asset
+    template_name = 'web/index.html'
+    context_object_name = 'assets'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(MainView, self).get_context_data()
+        context['title'] = 'Main page'
+        return context
+
+    def get_queryset(self):
+        return Asset.objects.filter(is_active=True).order_by('name')
