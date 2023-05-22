db7d20c40b2d5b1652dabcbfe972d2d2d92d99b4
dgolov <dmitriy.evgenyevich@gmail.com>
2023-05-22 14:53:58 +0300

    Updated context-title in CreateAssert view

--- web/views.py
+++ web/views.py
@@ -96,7 +96,7 @@ class CreateAssert(UserMixin, CreateView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(CreateAssert, self).get_context_data()
-        context['title'] = 'Create asset'
+        context['title'] = 'Создание актива'
         context['form'] = forms.CreateAssetForm
         return context
 
