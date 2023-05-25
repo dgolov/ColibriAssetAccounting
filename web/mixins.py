63b03da7748c72d6d7f04460f0ee7977de869280
dgolov <dmitriy.evgenyevich@gmail.com>
2023-05-25 21:00:06 +0300

    Added AssetMixin

--- web/mixins.py
+++ web/mixins.py
@@ -1,5 +1,85 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from typing import Union
+from web.models import Asset, History, Location
+from web.services import connect_to_redis
 
 
 class UserMixin(LoginRequiredMixin):
     login_url = '/auth'
+
+
+class AssetMixin:
+    try:
+        r = connect_to_redis()
+    except Exception as e:
+        print(e)
+        r = None
+
+    def save_old_asset_data(self, asset: Asset):
+        if not self.r:
+            return
+        self.r.set(f'{asset.pk}_name', asset.name, 120)
+        self.r.set(f'{asset.pk}_location', asset.location.pk, 120)
+        self.r.set(f'{asset.pk}_price', f"{asset.price}", 120)
+        self.r.set(f'{asset.pk}_state', f"{asset.state}", 120)
+        self.r.set(f'{asset.pk}_status', f"{asset.status}", 120)
+
+    def get_old_asset_data(self, asset_id: int) -> Union[dict, None]:
+        if not self.r:
+            return None
+        location_id = self.r.get(f'{asset_id}_location')
+        location = Location.objects.get(pk=location_id)
+
+        return {
+            "name": self.r.get(f'{asset_id}_name').decode(),
+            "location": location,
+            "price": float(self.r.get(f'{asset_id}_price')),
+            "state": int(self.r.get(f'{asset_id}_state')),
+            "status": self.r.get(f'{asset_id}_status').decode()
+        }
+
+    def create_asset_history(self, new_asset: Asset):
+        old_asset = self.get_old_asset_data(asset_id=new_asset.pk)
+        if not old_asset:
+            return None
+
+        if new_asset.name != old_asset.get('name'):
+            History.objects.create(
+                asset=new_asset,
+                old_name=old_asset.get('name'),
+                new_name=new_asset.name,
+                event_name="Изменение названия"
+            )
+
+        if new_asset.location != old_asset.get('location'):
+            History.objects.create(
+                asset=new_asset,
+                old_location=old_asset.get('location'),
+                new_location=new_asset.location,
+                event_name="Изменение склада"
+            )
+
+        if new_asset.price != old_asset.get('price'):
+            History.objects.create(
+                asset=new_asset,
+                old_price=old_asset.get('price'),
+                new_price=new_asset.price,
+                event_name="Изменение цены"
+            )
+
+        if new_asset.state != old_asset.get('state'):
+            History.objects.create(
+                asset=new_asset,
+                old_state=old_asset.get('state'),
+                new_state=new_asset.state,
+                event_name="Изменение состояния"
+            )
+
+        if new_asset.status != old_asset.get('status'):
+            History.objects.create(
+                asset=new_asset,
+                old_status=old_asset.get('status'),
+                new_status=new_asset.status,
+                event_name="Изменение статуса"
+            )
+
