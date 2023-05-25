a0167731b32863a509941905c2ef044bb483da4f
dgolov <dmitriy.evgenyevich@gmail.com>
2023-05-26 00:39:20 +0300

    Added logging for services

--- web/services.py
+++ web/services.py
@@ -1,9 +1,14 @@
 from django.conf import settings
 
+import logging
 import redis
 
 
+logger = logging.getLogger('main')
+
+
 def connect_to_redis():
+    logger.info("Try to connect redis")
     return redis.StrictRedis(
         host=settings.REDIS_HOST,
         port=settings.REDIS_PORT,
